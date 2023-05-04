cdc78230314b240b885b6242b0ef702140f93758
Andy Min <andrewkmin@gmail.com>
2023-05-03 21:47:17 -0500

    Add args for printing tokens and AST

--- src/main.rs
+++ src/main.rs
@@ -2,36 +2,51 @@ use colored::Colorize;
 use lc3_language_server::lexer::Lexer;
 use lc3_language_server::parser::Parser;
 use lc3_language_server::passes;
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
-    if let Some(file_name) = env::args().nth(1) {
-        let file_text = fs::read_to_string(&file_name).unwrap();
-        let file_lines = file_text.split("\n").collect::<Vec<&str>>();
+    let args = env::args().collect::<Vec<String>>();
 
-        let mut lexer = Lexer::from_text(&file_text);
-        let tokens = lexer.analyze();
+    if args.len() < 2 {
+        println!("Expected file name");
+        process::exit(1);
+    }
 
-        let mut parser = Parser::new(tokens);
-        let mut nodes = parser.parse_ast();
+    let file_name = &args[1];
+    let file_text = fs::read_to_string(&file_name).unwrap();
+    let file_lines = file_text.split("\n").collect::<Vec<&str>>();
 
-        passes::verify_labels(&mut nodes);
+    let mut lexer = Lexer::from_text(&file_text);
 
-        for node in &nodes {
-            for error in &node.errors {
-                println!("{}: {}", "error".red().bold(), error.bold());
-                println!(
-                    "{}:{}:{}\n",
-                    file_name, node.start_loc.line, node.start_loc.col
-                );
-                println!("\t{}", file_lines[node.start_loc.line - 1]);
-                println!(
-                    "\t{}{}",
-                    " ".repeat(node.start_loc.col - 1),
-                    "^".repeat(node.end_loc.col - node.start_loc.col + 1).red()
-                );
-                println!("");
-            }
+    let tokens = lexer.analyze();
+
+    if args.contains(&"--print-tokens".to_owned()) {
+        println!("{:#?}", tokens);
+    }
+
+    let mut parser = Parser::new(tokens);
+    let mut nodes = parser.parse_ast();
+
+    if args.contains(&"--print-ast".to_owned()) {
+        println!("{:#?}", nodes);
+    }
+
+    passes::verify_labels(&mut nodes);
+
+    for node in &nodes {
+        for error in &node.errors {
+            println!("{}: {}", "error".red().bold(), error.bold());
+            println!(
+                "{}:{}:{}\n",
+                file_name, node.start_loc.line, node.start_loc.col
+            );
+            println!("\t{}", file_lines[node.start_loc.line - 1]);
+            println!(
+                "\t{}{}",
+                " ".repeat(node.start_loc.col - 1),
+                "^".repeat(node.end_loc.col - node.start_loc.col + 1).red()
+            );
+            println!("");
         }
     }
 }
